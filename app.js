d2749157755b2a8401024174257a84a12532c9cc
Afnan Nizam <rookie26092003@gmail.com>
2023-03-02 22:05:12 +0530

    login with google

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 app.use(express.static('public'));
@@ -37,19 +39,55 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
 
     const userSchema = new mongoose.Schema({
         email:String,
-        passworsord :String
+        passworsord :String,
+        googleId:String
     });
 
     userSchema.plugin(passportLocalMongoose);
+    userSchema.plugin(findOrCreate);
 
     const User = mongoose.model('User', userSchema);
 
     passport.use(User.createStrategy());
-    passport.serializeUser(User.serializeUser());
-    passport.deserializeUser(User.deserializeUser());
+    passport.serializeUser(function(user, cb) {
+        process.nextTick(function() {
+          return cb(null,user.id);
+        });
+      });
+      
+      passport.deserializeUser(function(user, cb) {
+        process.nextTick(function() {
+          return cb(null, user);
+        });
+      });
+
+    passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        // console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
 
     app.get('/', (req, res) => {
         res.render('home');
+    });
+
+    app.get('/auth/google',
+        passport.authenticate('google', { scope: ["profile"] })
+    );
+
+    app.get('/auth/google/secrets', 
+        passport.authenticate('google', { failureRedirect: '/login' }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/secrets');
     });
 
     app.get('/login', (req, res) => {
@@ -69,8 +107,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
     })
 
     app.get('/logout', (req, res) => {
-        req.logout();
-        res.redirect('/');
+        req.logout(function(err) {
+            if (err) { 
+              return next(err); 
+              }
+            res.redirect('/');
+          });
     })
 
     app.post('/login', (req, res) => {
