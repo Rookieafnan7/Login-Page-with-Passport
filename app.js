c8ec08fa7cb29f8ea159dffc05ba681bef99bf3b
Afnan Nizam <rookie26092003@gmail.com>
2023-03-03 13:02:08 +0530

    login  with local google facebook methods

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
+
+
+// FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+// });
+
+
+// FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+// });
 
 const app = express();
 app.use(express.static('public'));
@@ -40,7 +51,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
     const userSchema = new mongoose.Schema({
         email:String,
         passworsord :String,
-        googleId:String
+        googleId:String,
+        facebookId:String,
+        secret: String
     });
 
     userSchema.plugin(passportLocalMongoose);
@@ -60,16 +73,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
           return cb(null, user);
         });
       });
-
+  
     passport.use(new GoogleStrategy({
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "http://localhost:3000/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
       },
       function(accessToken, refreshToken, profile, cb) {
         // console.log(profile);
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/secrets"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        // console.log(profile);
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
           return cb(err, user);
         });
       }
@@ -90,6 +116,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
             res.redirect('/secrets');
     });
 
+    app.get('/auth/facebook',
+        passport.authenticate('facebook'));
+
+    app.get('/auth/facebook/secrets',
+        passport.authenticate('facebook', { failureRedirect: '/login' }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/secrets');
+    });
+
     app.get('/login', (req, res) => {
         res.render('login');
     });
@@ -99,11 +135,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
     });
     
     app.get('/secrets', (req, res) => {
-        if(req.isAuthenticated()){
-            res.render('secrets');
-        }else{
-            res.redirect('/login');
-        }
+        // if(req.isAuthenticated()){
+        //     res.render('secrets');
+        // }else{
+        //     res.redirect('/login');
+        // }
+
+        User.find({"secret":{$ne: null}} , function(err,user){
+            if(err){
+                console.log(err)
+            }else{
+                if(user){
+                    res.render("secrets",{usersWithSecrets:user})
+                }
+            }
+        })
     })
 
     app.get('/logout', (req, res) => {
@@ -113,6 +159,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
               }
             res.redirect('/');
           });
+    })
+
+    app.get('/submit',(req, res) => {
+        if(req.isAuthenticated()){
+            res.render('submit');
+        }else{
+            res.redirect('/login');
+        }
+    })
+
+    app.post('/submit',(req,res)=>{
+        const submittedSecret = req.body.secret;
+        // console.log(req);
+        console.log(req.user);
+
+        User.findById(req.user,(err, user) => {
+            if(err){
+                console.log(err);
+            }else{
+                user.secret = submittedSecret;
+                user.save((err) => {
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log('secret saved');
+                        res.redirect('/secrets');
+                    }
+                })
+            }
+        })
     })
 
     app.post('/login', (req, res) => {
